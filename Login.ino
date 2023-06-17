f7ea4f3db353c23f8cbae9639300f5c293f278e2
Juan José Muñoz <94754375+Juanjos3m@users.noreply.github.com>
2023-06-17 10:57:12 -0500

    Login.ino

--- Login.ino
+++ Login.ino
@@ -1,12 +1,12 @@
 #include <LiquidCrystal.h>
 #include <Keypad.h>
 
-LiquidCrystal lcd(12, 11, 10, 9, 8, 7);
+LiquidCrystal lcd(12, 11, 6, 5, 4, 3);
 
 const byte KEYPAD_ROWS = 4;
 const byte KEYPAD_COLS = 4;
-byte rowPins[KEYPAD_ROWS] = {5, 4, 3, 2}; 
-byte colPins[KEYPAD_COLS] = {A3, A2, A1, A0};
+byte rowPins[KEYPAD_ROWS] = {35, 37, 39, 41}; 
+byte colPins[KEYPAD_COLS] = {43, 45, 47, 49};
 char keys[KEYPAD_ROWS][KEYPAD_COLS] = {
   {'1', '2', '3', '+'},
   {'4', '5', '6', '-'},
@@ -21,6 +21,9 @@ int contador = 0;
 char password [4];
 int count = 0;
 boolean bandera = true;
+int ledRojo = 16;
+int ledVerde = 18;
+int ledAzul = 19;
 
 void setup() {
   lcd.begin(16, 2);
@@ -31,6 +34,9 @@ void setup() {
   lcd.print("Ingrese clave");
   lcd.setCursor(4, 1);
 
+  pinMode(ledRojo, OUTPUT); // sets the redPin to be an output
+  pinMode(ledVerde, OUTPUT); // sets the greenPin to be an output
+  pinMode(ledAzul, OUTPUT); // sets the bluePin to be an output
 }
 
 void loop() {
@@ -48,6 +54,8 @@ void loop() {
           lcd.setCursor(0, 1);
           lcd.print("Correcta");
           delay(1000);
+          color(0,255, 0); // Led verde
+          delay(1000); 
           lcd.clear();
           count = 3;
           bandera = true;
@@ -60,6 +68,8 @@ void loop() {
           lcd.print("Incorrecta");
           count = count + 1;
           delay(1000);
+          color(255, 0, 0); Led rojo
+          delay(1000); 
           lcd.clear();
           if(count < 3){
               lcd.setCursor(0, 0);
@@ -75,7 +85,10 @@ void loop() {
           lcd.print("Sistema");
           lcd.setCursor(0, 1);
           lcd.print("bloqueado");
+          delay(1000);
+          color(0, 0, 255); // Led azul 
+          delay(1000); 
       }
     }
+    
   }
-
